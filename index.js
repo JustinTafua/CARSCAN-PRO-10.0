de996c3d317669beadc0db686dd90a8e973bc3c5
JustinTafua <j.tafua1990@gmail.com>
2025-09-22 05:29:07 -0500

    src/
    pages

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home(){
+  return (
+    <main style={{fontFamily:'sans-serif',textAlign:'center',padding:'2rem'}}>
+      <h1>CarScan Pro</h1>
+      <p>Created by Justin Tafua</p>
+      <a href="/scan">Go to Scanner</a>
+    </main>
+  );
+}
