5f2747ceb701e6a1b9f7cb03c817623f8b83b799
JustinTafua <j.tafua1990@gmail.com>
2025-09-22 17:45:42 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home(){
-  return (
-    <main style={{fontFamily:'sans-serif',textAlign:'center',padding:'2rem'}}>
-      <h1>CarScan Pro</h1>
-      <p>Created by Justin Tafua</p>
-      <a href="/scan">Go to Scanner</a>
-    </main>
-  );
-}
